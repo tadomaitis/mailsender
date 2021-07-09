78c30c0a8d806238f43218daf41c86f16ae64500
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-09 14:23:25 -0300

    changes function to use gmail API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,28 +2,60 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import { createTransport } from 'nodemailer'
 import * as cors from 'cors'
+import { google } from 'googleapis'
 
 admin.initializeApp()
 const corsHandler = cors({ origin: true })
 
-const EMAIL_HOST = functions.config().email.host
-const EMAIL_ACCOUNT = functions.config().email.account
-const EMAIL_PORT = functions.config().email.port
-const PASSWORD = functions.config().email.password
+interface EmailOptions {
+  subject: string
+  text: string
+  to: string
+  html: string
+  from: string | undefined
+}
 
-const transporter = createTransport({
-  host: EMAIL_HOST,
-  port: EMAIL_PORT,
-  secure: true,
-  auth: {
-    user: EMAIL_ACCOUNT,
-    pass: PASSWORD
-  }
-})
+const OAuth2 = google.auth.OAuth2
+const OAUTH_URI = 'https://developers.google.com/oauthplayground'
+const CLIENT_ID = functions.config().email.client_id
+const CLIENT_SECRET = functions.config().email.client_secret
+const REFRESH_TOKEN = functions.config().email.refresh_token
+const EMAIL_ACCOUNT = functions.config().email.account
 
 export const sendMail = functions.https.onRequest((req, res) => {
-  corsHandler(req, res, () => {
-    const mailData = {
+  corsHandler(req, res, async () => {
+    const createTransporter = async () => {
+      const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, OAUTH_URI)
+
+      oauth2Client.setCredentials({
+        refresh_token: REFRESH_TOKEN
+      })
+
+      const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+            reject(new Error(`Failed to create access token: ${err}`))
+          }
+          resolve(token)
+        })
+      })
+
+      const transporter = createTransport({
+        // @ts-ignore
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: EMAIL_ACCOUNT,
+          accessToken,
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN
+        }
+      })
+      return transporter
+    }
+
+    const emailOptions: EmailOptions = {
       from: `${req.body.email}`,
       to: 't.d.adomaitis@gmail.com',
       subject: `Mensagem recebida no site de ${req.body.name}`,
@@ -34,15 +66,13 @@ export const sendMail = functions.https.onRequest((req, res) => {
           <div>${req.body.message}</div>
         `
     }
-    transporter.sendMail(mailData, err => {
-      if (err) {
-        console.log(err)
-        return res
-          .status(500)
-          .send(err.toString())
-          .end()
-      }
-      return res.status(200).end()
-    })
+
+    const sendEmail = async (emailOptions: EmailOptions) => {
+      const emailTransporter = await createTransporter()
+      await emailTransporter.sendMail(emailOptions)
+    }
+
+    sendEmail(emailOptions)
+    return res.status(200).end()
   })
 })
